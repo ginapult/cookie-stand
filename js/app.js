54011cbe417c70b236cfc444e9d1c59e8798e6c1
ginapult <gina.pultorak@gmail.com>
2019-06-10 19:22:57 -0700

    updated the 11am with ones

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Global variable - time
-var times = ['6am','7am','8am','9am','10am','llam','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+var times = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 
 //Var location 1st & Pike
