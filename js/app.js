c509f77216e257bdb3d9d8eb5973af3d213db981
ginapult <gina.pultorak@gmail.com>
2019-06-12 17:54:57 -0700

    updated sales table includes stretch goal

--- js/app.js
+++ js/app.js
@@ -2,83 +2,124 @@
 
 //Global variable - time
 var times = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-
-function Location(name, min, max, avgCookies) {
+var hourTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var listOfAllStores = [];
+console.log(listOfAllStores);
+function Stores(name, min, max, avgCookies) {
   this.name = name;
   this.min = min;
   this.max = max;
   this.avgCookies = avgCookies;
   this.avgCookiesPerHourArray = [];
   this.sumCookies = 0;
-  Location.list.push(this);
+  listOfAllStores.push(this);
 
-  Location.avgCookiesPerHour(this);
+  Stores.avgCookiesPerHour(this);
 }
-Location.list = [];
 
-Location.prototype.randomCustsPerHour = function (min,max) {
+Stores.prototype.randomCustsPerHour = function (min,max) {
   return Math.floor(Math.random() *
     (max - min + 1)) + min;
 };
 
-Location.avgCookiesPerHour = function (locationObject) {
+Stores.avgCookiesPerHour = function (StoresObject) {
   for (var i = 0; i < times.length; i++) {
-    var calculatedCookies = Math.ceil(locationObject.randomCustsPerHour(locationObject.min,locationObject.max) * locationObject.avgCookies);
-    locationObject.avgCookiesPerHourArray.push(calculatedCookies);
-    locationObject.sumCookies += calculatedCookies;
+    var calculatedCookies = Math.ceil(StoresObject.randomCustsPerHour(StoresObject.min,StoresObject.max) * StoresObject.avgCookies);
+    StoresObject.avgCookiesPerHourArray.push(calculatedCookies);
+    StoresObject.sumCookies += calculatedCookies;
+    hourTotals[i] += StoresObject.sumCookies;
   }
 };
 
-function renderTable() {
-
-//This section is building the header row of the table
-
-  // Location.prototype.render = function () {
-  var tableHeader = document.getElementById('tableHeader');
+Stores.prototype.renderRow = function () {
   var tableBody = document.getElementById('tableBody');
-  var tableFooter = document.getElementById('tableFooter');
   var tableRow = document.createElement('tr');
-  var cell = document.createElement('td');
+  var tableData = document.createElement('td');
+  tableData.textContent = this.name;
+  tableRow.appendChild(tableData);
+  for (var i = 0; i < times.length; i++) {
+    var td = document.createElement('td');
+    td.textContent = this.avgCookiesPerHourArray[i];
+    tableRow.appendChild(td);
+  }
+
+  tableBody.appendChild(tableRow);
+  var tdTotal = document.createElement('td');
+  tdTotal.textContent = this.sumCookies;
+  tableRow.appendChild(tdTotal);
+
+};
+
+
+function createHeader () {
+  var tableHeader = document.getElementById('tableHeader');
+  var tableRow = document.createElement('tr');
 
   //This is creating the first empty cell of the header
   var firstColumn = document.createElement('th');
   firstColumn.textContent = '';
   tableRow.appendChild(firstColumn);
 
-  //This is creating the "times" cells in the header row between the blank cell and the "Total" cell
+  //This is creating the "times" cells in the header row between the blank cell and the "Daily Stores Totals" cell
   for (var i = 0; i < times.length; i++) {
     var tableHead = document.createElement('th');
     tableHead.textContent = times[i];
     tableRow.appendChild(tableHead);
   }
+
   //This is creating the last cell in the header with "Total" in it
   var lastColumn = document.createElement('th');
-  lastColumn.textContent = 'Total';
+  lastColumn.textContent = 'Daily Stores Totals';
   tableRow.appendChild(lastColumn);
- 
+
   tableHeader.appendChild(tableRow);
-
-//This section is building the body of the table
-
-  for (i = 0; i < Location.list.length; i++) {
-    tableRow = document.createElement('tr');
-    var storeName = document.createElement('td');
-    storeName.textContent = Location.list[i].name;
-    tableRow.appendChild(storeName);
-
-    for (var t = 0; t < times.length; t++) {
-      var timeCookies = document.createElement('td');
-      timeCookies.textContent = Location.list[i].avgCookiesPerHourArray[t];
-      tableRow.appendChild(timeCookies);
-    }
-    tableBody.appendChild(tableRow);
-  }
 }
 
-new Location ('1st & Pike', 23 , 65, 6.3);
-new Location ('SeaTac Airport', 3, 24, 1.2);
-new Location ('Seattle Center', 11, 38, 3.7);
-new Location ('Capitol Hill', 20, 38, 2.3);
-new Location ('Alki', 2, 16, 4.6);
+function createFooter () {
+  console.log('hour totals',hourTotals);
+  var tableFooter = document.getElementById('tableFooter');
+  var tableRow = document.createElement('tr');
 
-renderTable();
+  var firstCellFooter = document.createElement('td');
+  firstCellFooter.textContent = 'Total';
+  tableRow.appendChild(firstCellFooter);
+
+  for (var i = 0; i < hourTotals.length; i++) {
+    var tableFoot = document.createElement('td');
+    tableFoot.textContent = hourTotals[i];
+    tableRow.appendChild(tableFoot);
+  }
+
+  var lastCellFooter = document.createElement('td');
+  
+  var sum = 0;
+  for (var k = 0; k < hourTotals.length; k++) {
+    sum += hourTotals[k];
+  }
+  lastCellFooter.textContent = sum;
+  tableRow.appendChild(lastCellFooter);
+
+
+  tableFooter.appendChild(tableRow);
+
+}
+
+
+
+var Pike = new Stores ('1st & Pike', 23 , 65, 6.3);
+var SeaTac = new Stores ('SeaTac Airport', 3, 24, 1.2);
+var SeattleCenter= new Stores ('Seattle Center', 11, 38, 3.7);
+var CapitolHill = new Stores ('Capitol Hill', 20, 38, 2.3);
+var Alki = new Stores ('Alki', 2, 16, 4.6);
+
+Pike.renderRow();
+SeaTac.renderRow();
+SeattleCenter.renderRow();
+CapitolHill.renderRow();
+Alki.renderRow();
+
+createHeader();
+createFooter();
+console.log(hourTotals);
+
+
